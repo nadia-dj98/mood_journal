2c6cb8eb2bfda487cad75328bd60be465ac3462f
nadia <nadia_dj@hotmail.co.uk>
2023-03-13 10:58:19 +0000

    created repo.

---
+++ mood_prompt_journal/mood_prompt_journal/src/main/java/com/example/mood_prompt_journal/MoodPromptJournalApplication.java
@@ -0,0 +1,13 @@
+package com.example.mood_prompt_journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodPromptJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodPromptJournalApplication.class, args);
+	}
+
+}
---
+++ mood_prompt_journal/mood_prompt_journal/src/test/java/com/example/mood_prompt_journal/MoodPromptJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mood_prompt_journal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodPromptJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
